c367ab114769550dcd9087e233eb4e7dcc1601ff
Roel van Hoof <r.v.hoof@gmail.com>
2018-12-31 12:51:35 +0100

    cleanup code

--- lib/code_style_configuration.rb
+++ lib/code_style_configuration.rb
@@ -2,5 +2,4 @@ require "code_style_configuration/version"
 require "code_style_configuration/railtie" if defined?(Rails)
 
 module CodeStyleConfiguration
-  # Your code goes here...
 end
--- spec/code_style_configuration_spec.rb
+++ spec/code_style_configuration_spec.rb
@@ -4,8 +4,4 @@ describe CodeStyleConfiguration do
   it 'has a version number' do
     expect(CodeStyleConfiguration::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
